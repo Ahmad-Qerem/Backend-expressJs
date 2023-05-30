6bd4d8830eb40ba560f87a575a65e9f142ef5d5e
ahmad-qerem <alqerema@gmail.com>
2023-05-30 23:30:35 +0300

    add news

---
+++ src/Modules/Api/News/Controllers/newsControllers.js
@@ -0,0 +1,69 @@
+import createError from "http-errors";
+import {
+  getAllNews,
+  createNew,
+  getNew,
+  updateNew,
+  deleteNew,
+} from "../Services/NewsServices.js";
+
+const getAllNewsController = async (req, res, next) => {
+  try {
+    const News = await getAllNews();
+    res.send(News);
+  } catch (error) {
+    next(createError(error));
+  }
+};
+
+const createNewController = async (req, res, next) => {
+  try {
+    const data = req.body;
+    const userId = req.user.id;
+    const newNews = await createNew(data,userId);
+    res.send(newNews);
+  } catch (error) {
+    next(createError(error));
+  }
+};
+
+const getNewController = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const news = await getNew(id);
+    res.send(news);
+  } catch (error) {
+    next(createError(error));
+  }
+};
+const updateNewController = async (req, res, next) => {
+  try {
+    const data = req.body;
+
+    const news = await updateNew(data);
+    res.send(news);
+  } catch (error) {
+    next(createError(error));
+  }
+};
+
+const deleteNewController = async (req, res, next) => {
+  try {
+    const id = parseInt(req.params.id);
+    const deletedTransaction = await deleteNew(id);
+    res.send({
+      success: true,
+      message: deletedTransaction,
+    });
+  } catch (error) {
+    next(createError(error));
+  }
+};
+
+export  {
+  getAllNewsController,
+  createNewController,
+  getNewController,
+  updateNewController,
+  deleteNewController,
+};
---
+++ src/Modules/Api/News/Routers/news.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { authenticateToken } from "../../../Middleware/Auth/JWT.js";
+import {
+  getAllNewsController,
+  createNewController,
+  getNewController,
+  updateNewController,
+  deleteNewController,
+} from "../Controllers/newsControllers.js";
+
+const router = express.Router();
+router.get("", authenticateToken, getAllNewsController);
+router.New("", authenticateToken, createNewController);
+router.get("/:id", authenticateToken, getNewController);
+router.put("/:id", authenticateToken, updateNewController);
+router.delete("/:id", authenticateToken, deleteNewController);
+
+export default router;
